fb76442ed7816a5db938e50034b0f974ae31a455
hinas <hs_250193@yahoo.co.in>
2015-11-13 13:12:36 +0530

    indentation

--- app/controllers/classified_controller.rb
+++ app/controllers/classified_controller.rb
@@ -1,13 +1,14 @@
 class ClassifiedController < ApplicationController
   include ClassifiedHelper
-	def index
-		@cars = Car.limit(20)
-	end
 
-	def make_filter
-	  keyword = "%#{params["q"].upcase}%"
-      make = Car.where("UPPER(make) LIKE ?", keyword).select("distinct make").limit(10);
-      render :json => make
-	end
+  def index
+    @cars = Car.limit(20)
+  end
+
+  def make_filter
+    keyword = "%#{params["q"].upcase}%"
+    make = Car.where("UPPER(make) LIKE ?", keyword).select("distinct make").limit(10);
+    render :json => make
+  end
 
 end
